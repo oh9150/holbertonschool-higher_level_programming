68768c5cf63c1f6ef315157cd8e9ea0f673f91af
root <6351@holbertonstudents.com>
2023-05-31 21:10:48 -0700

    add, pow, fizzbuzz

---
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def fizzbuzz:
+    for i in range(100):
+        if (i % 3 == 0):
+            print("Fizz")
+        if (i % 5 == 0):
+            print("Buzz")
+        if (i % 5 != 0 or i % 3 != 0):
+            print("{}".format(i))
