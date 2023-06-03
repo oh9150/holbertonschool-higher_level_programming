e1706da6fbc22edfde83f5d0eaf1705b531e35d5
root <6351@holbertonstudents.com>
2023-06-02 19:03:11 -0700

    fix

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     import hidden_4
 
     names = dir(hidden_4)
-    for i in names:
+    for name in names:
         if name[:2] != "__":
-            print(i)
+            print(name)
