619ae19f0993dedf857804fc4027d74ec2c69772
root <6351@holbertonstudents.com>
2023-06-02 22:01:38 -0700

    calculator exit

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from sys import argv
+    import sys
     from calculator_1 import add, sub, mul, div
 
+    argv = sys.argv
     args = len(argv) - 1
     a = int(argv[1])
     b = int(argv[3])
@@ -11,7 +12,7 @@ if __name__ == "__main__":
 
     if args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
+        sys.exit(1)
 
     if op == "+":
         print("{} {} {} = {}".format(a, op, b, add(a, b)))
@@ -23,4 +24,4 @@ if __name__ == "__main__":
         print("{} {} {} = {}".format(a, op, b, div(a, b)))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit(1)
+        sys.exit(1)
