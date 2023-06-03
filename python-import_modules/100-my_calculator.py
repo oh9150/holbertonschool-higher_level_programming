34fbac30ac4c9408f3a0eda5aa98e7be49fe76a9
root <6351@holbertonstudents.com>
2023-06-02 22:36:51 -0700

    my calculator fix

--- python-import_modules/100-my_calculator.py
+++ python-import_modules/100-my_calculator.py
@@ -5,14 +5,15 @@ if __name__ == "__main__":
 
     argv = sys.argv
     args = len(argv) - 1
-    a = int(argv[1])
-    b = int(argv[3])
-    op = argv[2]
-    state = False
 
     if args != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
+    a = int(argv[1])
+    b = int(argv[3])
+    op = argv[2]
+    state = False
 
     if op == "+":
         print("{} {} {} = {}".format(a, op, b, add(a, b)))
