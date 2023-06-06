58c3929f35367a832d179c3a7abf4ae734e48fb1
root <6351@holbertonstudents.com>
2023-06-05 20:41:33 -0700

    fixes

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,5 +2,5 @@
 def new_in_list(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return my_list[:]
-    copy = my_list[:]
-    copy[idx] = element
+    new = my_list.copy
+    new[idx] = element
--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    for i in range(len(my_string) - 1):
-        if (my_string[i] != "c" and my_string[i] != "c"):
-            new += my_string[i]
+    my_string.translate({ord('c'): None for i in "cC"})
     return new
