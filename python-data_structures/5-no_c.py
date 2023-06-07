1dd4fdf75bf06a1a961f4baa067aeea38a920cb2
root <6351@holbertonstudents.com>
2023-06-06 21:18:46 -0700

    fix

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    my_string = my_string.translate({ord("cC"): None})
+    my_string = my_string.translate({ord(i): None for i in "cC"})
     return my_string
