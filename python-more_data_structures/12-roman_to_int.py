3e9b704ea8754f40844e54713cfbe7faa21b01c2
root <6351@holbertonstudents.com>
2023-06-09 21:07:24 -0700

    code stylr

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     num = 0
-    if isinstance(roman_string, str) and not roman_string == None:
-        numerals = {"M": 1000, "D": 500, "C": 100, 
-                "L": 50, "X": 10, "V": 5, "I": 1}
+    if isinstance(roman_string, str) and roman_string is not None:
+        numerals = {
+                "M": 1000,
+                "D": 500,
+                "C": 100,
+                "L": 50,
+                "X": 10,
+                "V": 5,
+                "I": 1
+                }
         for i, c in enumerate(roman_string):
             num += numerals[c]
             if i < len(roman_string) - 1:
