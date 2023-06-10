bfafbac03b5ef5408156887a1327d3c3e85b7546
root <6351@holbertonstudents.com>
2023-06-09 18:34:14 -0700

    fixes

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             retval += 1
         except (ValueError, TypeError):
-            pass
+            continue
         except IndexError:
             break
     print("")
