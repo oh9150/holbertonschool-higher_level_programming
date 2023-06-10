37850f407e0ad594016b1d35c33dc9c65d0e32de
root <6351@holbertonstudents.com>
2023-06-09 21:21:26 -0700

    weight average

--- python-more_data_structures/100-weight_average.py
+++ python-more_data_structures/100-weight_average.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    average = 0
+    average = size = 0
     for item in my_list:
         average += item[0] * item[1]
-    average /= my_list.sum()
-    return average
+        size += item[1]
+    return average / size
