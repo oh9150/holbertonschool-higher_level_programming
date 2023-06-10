c310c8018bd318b2fe0b75b0a36ecea2fc0484d3
root <6351@holbertonstudents.com>
2023-06-09 20:50:47 -0700

    search replace

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
+    if not isinstance(my_list, list):
+        return None
     new_list = my_list.copy()
     for element in new_list:
         if element == search:
