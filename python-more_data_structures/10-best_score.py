fc9f22754cc503caae9f5a4d5690cb6d4d46a274
root <6351@holbertonstudents.com>
2023-06-09 20:47:42 -0700

    best score fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    key = list(a_dictionary.keys)[0]
+    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+        return None
+    key = list(a_dictionary.keys())[0]
     val = a_dictionary[key]
     for k, v in a_dictionary.items():
         if v > val:
