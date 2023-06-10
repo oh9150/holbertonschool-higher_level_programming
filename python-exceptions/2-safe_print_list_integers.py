79299c5758d8b71f9a05b862e832b47a1c24a116
root <6351@holbertonstudents.com>
2023-06-09 18:35:49 -0700

    fix?

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     retval = 0
-    for i in range(x):
+    for i in range(x - 1):
         try:
             print("{:d}".format(my_list[i]), end="")
             retval += 1
