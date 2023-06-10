7e537dd36d26a7388419be90624309546a9ece87
root <6351@holbertonstudents.com>
2023-06-09 22:03:14 -0700

    complex delete fix

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     for k, v in a_dictionary.items():
-        if a_dictionary[k] == value:
+        if v == value:
             del a_dictionary[k]
     return a_dictionary
