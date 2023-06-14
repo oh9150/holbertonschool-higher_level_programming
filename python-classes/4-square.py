a22c1d47a60e0d54fbd35cd28fb0a50f604d81fa
root <6351@holbertonstudents.com>
2023-06-13 18:45:51 -0700

    getter/setter

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,15 +7,33 @@ class Square:
         Args:
             size (int): The size of the square
         """
-        if isinstance(size, int):
-            if size < 0:
-                raise ValueError("size must be >= 0")
-            self.__size = size
-        else:
-            raise TypeError("size must be an integer")
+        self.size(size)
+
     def area(self):
         """Returns the current square area
         Returns:
             The current area of the square
         """
         return self.__size ** 2
+
+    @property
+    def size(self):
+        """Gets the size of the square
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square
+        Args:
+            value (int): The size to set the square
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
