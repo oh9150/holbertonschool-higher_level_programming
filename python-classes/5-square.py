559a2a32dadef95d8610a374fcc4681a1346b7b2
root <6351@holbertonstudents.com>
2023-06-13 19:07:25 -0700

    square 5

---
+++ python-classes/5-square.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python3
+"""Defines a class Square"""
+class Square:
+    """Defines a square"""
+    def __init__(self, size=0):
+        """Initializes the square
+        Args:
+            size (int): The size of the square
+        """
+        self.size = size
+
+    def area(self):
+        """Returns the current square area
+        Returns:
+            The current area of the square
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """Gets the size of the square
+        Returns:
+            The size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square
+        Args:
+            value (int): The size to set the square
+        """
+        if isinstance(value, int):
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+
+    def my_print(self):
+        """Prints the square"""
+        for height in range(self.__size):
+            for width in range(self.__size):
+                print("#")
+            print("")
+        if self.__size == 0:
+            print("")
