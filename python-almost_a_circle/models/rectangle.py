2e40f141bcb922c9a678105bcc9d6b96d36e3024
Mateo Herrera <mateocreper038@gmail.com>
2023-06-26 17:20:15 -0700

    rectangle #1

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from Base"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """Defines a rectangle"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Instantiates a rectangle
+        Args:
+            width (int): The width of the rectangle
+            height (int): The height of the rectangle
+            x (int): The x position of the rectangle
+            y (int): The y position of the rectangle
+            id (int): The id of the rectangle
+        """
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Getter for the width property"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        self.__width = value
+    
+    @property
+    def height(self):
+        """Getter for the height property"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for the x property"""
+        return self.__x
+
+    @x.setter
+    def width(self, value):
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for the y property"""
+        return self.__y
+
+    @y.setter
+    def width(self, value):
+        self.__y = value
