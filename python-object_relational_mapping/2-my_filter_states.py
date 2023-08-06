4879579a45f3cfc3babf61af3ba239fcca447132
root <6351@holbertonstudents.com>
2023-08-05 21:30:24 -0700

    fix?

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -9,9 +9,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states")
+    cur.execute("SELECT * FROM states
+                 WHERE name = {}".format(sys.argv[4]))
     for state in cur.fetchall():
-        if state[1] == sys.argv[4]:
-            print(state)
+        print(state)
     cur.close()
     db.close()
