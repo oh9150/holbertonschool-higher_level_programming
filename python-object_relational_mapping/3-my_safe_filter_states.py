006856b90ade566506436164666b885f0b358e64
root <6351@holbertonstudents.com>
2023-08-05 21:50:24 -0700

    safe filter

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -9,8 +9,9 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE BINARY name = '{}'".format(sys.argv[4]))
+    cur.execute("SELECT * FROM states")
     for state in cur.fetchall():
-        print(state)
+        if state[1] == sys.argv[4]:
+            print(state)
     cur.close()
     db.close()
