11c07fc6dff2412fa7ac6ef6a2bdebff9ce3ebca
root <6351@holbertonstudents.com>
2023-08-05 21:12:54 -0700

    why it no worky :(

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -8,5 +8,4 @@ if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = db.cursor()
     cur.execute("SELECT * FROM states")
-    for state in cur.fetchall():
-        print(state)
+    [print(state) for state in cur.fetchall()]
