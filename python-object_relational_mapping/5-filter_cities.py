7a6b363b2354f9e5fef201954eacfcc93b99e03d
root <6351@holbertonstudents.com>
2023-08-05 23:56:28 -0700

    filter cities

---
+++ python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Lists all cities in a certain state from the database hbtn_03_4_usa
+Usage: ./5-filter_cities.py <mysql user> <mysql pass> <db> <state name>
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name, states.name \
+               FROM cities \
+               INNER JOIN states ON cities.state_id = states.id \
+               ORDER BY cities.id ASC")
+    print(", ".join(
+        city[0] for city in cur.fetchall() if city[1] == sys.argv[4]))
