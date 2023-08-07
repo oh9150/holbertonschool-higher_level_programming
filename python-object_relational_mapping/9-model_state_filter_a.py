59cb19bdb31f294ef89a91f2ab0ab5d8ce89f4e2
root <6351@holbertonstudents.com>
2023-08-06 21:46:40 -0700

    state filter

---
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Lists all State objects from the database hbtn_0e_6_usa
+Usage: ./7-model_state_fetch_all <mysql user> <mysql passwd> <db>
+"""
+import sys
+from sqlalchemy import create_engine
+from model_state import State
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id).all()
+    for state in states:
+        if a in state.name:
+            print("{}: {}".format(state.id, state.name))
